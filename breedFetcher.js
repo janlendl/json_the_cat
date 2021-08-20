722e0acacb6e4125c28d403ce0d46364909b9748
Lendl Peralta <janlendl@gmail.com>
2021-08-20 20:35:58 +0000

    added edge case where it will return error if breed is not found

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,12 +2,15 @@ const request = require('request');
 const catBreed = process.argv.slice(2);
 
 request(`https://api.thecatapi.com/v1/breeds/search?q=${catBreed}`, (error, response, body) => {
-  if(error || response.statusCode !== 200) {
+  if (error || response.statusCode !== 200) {
     console.log(`Error ${response.statusCode}, please check again!`);
     return;
   }
   const data = JSON.parse(body);
+  if (data.length === 0) {
+    console.log(`${catBreed} type of breed NOT FOUND!`);
+  }
   for (const keys in data) {
     console.log(data[keys].description);
-    }
+  }
 });
