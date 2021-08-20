89cb9ad5599ef741e04c7a3fbfe7757a1fea2055
Lendl Peralta <janlendl@gmail.com>
2021-08-20 19:31:20 +0000

    updated to be able to dynamically search for the breed and also edge case for request failed errors

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,9 +1,13 @@
 const request = require('request');
-const catBreed = process.argv.slice(1);
+const catBreed = process.argv.slice(2);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=siberian`, (error, response, body) => {
+request(`https://api.thecatapi.com/v1/breeds/search?q=${catBreed}`, (error, response, body) => {
+  if(error || response.statusCode !== 200) {
+    console.log(`Error ${response.statusCode}, please check again!`);
+    return;
+  }
   const data = JSON.parse(body);
   for (const keys in data) {
     console.log(data[keys].description);
-  }
+    }
 });
