b31e8a538ad7bcec58700da50f0f21d2c7691253
Lendl Peralta <janlendl@gmail.com>
2021-08-20 20:37:37 +0000

    added line comments

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,15 +2,15 @@ const request = require('request');
 const catBreed = process.argv.slice(2);
 
 request(`https://api.thecatapi.com/v1/breeds/search?q=${catBreed}`, (error, response, body) => {
-  if (error || response.statusCode !== 200) {
+  if (error || response.statusCode !== 200) { // generate error message if error and status is not 200
     console.log(`Error ${response.statusCode}, please check again!`);
     return;
   }
   const data = JSON.parse(body);
-  if (data.length === 0) {
+  if (data.length === 0) { // this will check if breed is in the result
     console.log(`${catBreed} type of breed NOT FOUND!`);
   }
-  for (const keys in data) {
+  for (const keys in data) { // for loop to iterate on the objects to get the needed key value
     console.log(data[keys].description);
   }
 });
