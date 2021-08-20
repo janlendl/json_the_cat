6f8f1af313634c8f060fffec72f488e74974be86
Lendl Peralta <janlendl@gmail.com>
2021-08-20 22:34:18 +0000

    updated breedFetcher file to include it inside a function and moved out the breedname call and print out to a different file

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,20 @@
 const request = require('request');
-const catBreed = process.argv.slice(2);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${catBreed}`, (error, response, body) => {
-  if (error || response.statusCode !== 200) { // generate error message if error and status is not 200
-    console.log(`Error ${response.statusCode}, please check again!`);
-    return;
-  }
-  const data = JSON.parse(body);
-  if (data.length === 0) { // this will check if breed is in the result
-    console.log(`${catBreed} type of breed NOT FOUND!`);
-  }
-  for (const keys in data) { // for loop to iterate on the objects to get the needed key value
-    console.log(data[keys].description);
-  }
-});
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error || response.statusCode !== 200) { // generate error message if error and status is not 200
+      callback(error, null);
+      // console.log(`Error ${response.statusCode}, please check again!`);
+      return;
+    }
+    const data = JSON.parse(body);
+    if (data.length === 0) { // this will check if breed is in the result
+      callback(`${breedName} type of breed NOT FOUND!`, null);
+    }
+    for (const keys in data) { // for loop to iterate on the objects to get the needed key value
+      callback(null, data[keys].description);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
